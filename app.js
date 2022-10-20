4e7e9dbf9362bcd7cdf9173c0e5d0d65b47c7ab8
Richard95360 <richard.haddad1@free.fr>
2022-10-20 13:36:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,58 @@
+document.addEventListener('alpine:init', () => {
+
+    Alpine.directive('active-page', (el, {expression}, {effect, evaluateLater}) => {
+        const onActiveChange = evaluateLater(expression)
+
+        effect(() => {
+            onActiveChange(active => {
+              if(active) {
+                el.classList.add('active')
+                el.setAttribute('aria-current', 'page')
+              }else {
+                el.classList.remove('active')
+                el.removeAttribute('aria-current')
+              }
+            })
+
+        })
+
+    })
+
+    Alpine.data('tabs', (defaultTab) => ({
+            tab: defaultTab,
+            toggleTab (e) {
+              this.tab = e.target.getAttribute('href').replace('#', '')
+            },
+            isActive (tab){
+                return tab === this.tab
+            }
+ 
+        }))
+
+        Alpine.store('posts',  {
+            loading: false,
+            posts:[],
+            loaded:false,
+             loadPosts () {
+                if(this.loaded) {
+                    return;
+                }
+                this.loading = true
+               fetch('https://jsonplaceholder.typicode.com/posts?_limit=4')
+               .then(r => r.json())
+               .then(json =>{ 
+                this.posts = json
+                this.loaded = true
+                this.loading = false
+            
+            })
+               
+            }
+        })
+        Alpine.data('posts', () => ({
+            init() {
+               this.$store.posts.loadPosts()
+            }
+        }))
+    
+})
